3b087c2738b8650b11c7a6e6572bea67a7bd8f26
mjokarr <mo.jokar80@gmail.com>
2024-05-18 00:17:24 +0330

    merge UpdateVideoRequest with StoreVideoRequest and decrease repeat codes

--- app/Http/Requests/UpdateVideoRequest.php
+++ app/Http/Requests/UpdateVideoRequest.php
@@ -6,36 +6,13 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
 
-class UpdateVideoRequest extends FormRequest
+class UpdateVideoRequest extends StoreVideoRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
-        return [
-            'name' => ['required'],
-            'length' => ['required', 'integer'],
+        return array_merge(parent::rules(), [
             'slug' => ['required', Rule::unique('videos')->ignore($this->video), 'alpha_dash'],
-            'url' => ['required'],
-            'thumbnail' => ['required', 'url']
-        ];
-    }
-
-    protected function prepareForValidation(): void
-    {
-        $this->merge([
-            'slug' => Str::slug($this->slug),
         ]);
     }
 }
